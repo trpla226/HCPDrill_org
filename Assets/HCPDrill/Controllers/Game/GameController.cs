a569c57e06da1aaf5311e7aa81720d5c1f95b735
trpla226 <trpla226@hotmail.co.jp>
2020-12-17 00:54:00 +0900

    refactor: Sessionクラス導入
    ゲームの流れの制御をGameController、ゲームルールをSessionに分離しようとしている。が、あまりきれいに分けられていない。
    
    デリゲートを使って、AreaController系のクラスをSession自体が知らなくていいようにすべきか。

--- Assets/HCPDrill/Controllers/Game/GameController.cs
+++ Assets/HCPDrill/Controllers/Game/GameController.cs
@@ -20,17 +20,8 @@ public class GameController : MonoBehaviour
     public int InitialTimeLimitSeconds = 30;
     public float wrongAnswerPenaltySeconds = 1f;
 
-    // 手札枚数
-    const int dealCount = 13;
-
-    // 乱数シード
-    int seed;
-
     // 現在の手札
-    private Hand Hand;
-    int answer;
-    private int score;
-    private float currentTimeLimitSeconds;
+    private Session session;
     bool playerHasAnswered = false;
 
     // 表示制御
@@ -47,7 +38,6 @@ public class GameController : MonoBehaviour
     private GameObject correctOverlay;
 
     private TimeLimitAreaController timeLimitAreaController;
-    private bool answerIsCorrect;
 
 
     #endregion
@@ -57,35 +47,36 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     IEnumerator Start()
     {
-        seed = DateTime.Now.Millisecond;
-
-        currentTimeLimitSeconds = InitialTimeLimitSeconds;
-        score = 0;
-
-        // 音声
-        audioSource = GetComponent<AudioSource>();
-
         // 参照キャッシュ
+        correctOverlay = GameObject.Find("Correct");
+
         answerButtonController = FindObjectOfType<AnswerButtonController>();
         handController = FindObjectOfType<HandController>();
-        correctOverlay = GameObject.Find("Correct");
         timeLimitAreaController = GameObject.Find("TimeLimitArea").GetComponent<TimeLimitAreaController>();
         scoreAreaController = GameObject.Find("ScoreArea").GetComponent<ScoreAreaController>();
+
+        // このオブジェクトのコンポーネント
+        audioSource = GetComponent<AudioSource>();
+
+        session = new Session(scoreAreaController, 
+            timeLimitAreaController,
+            InitialTimeLimitSeconds,
+            wrongAnswerPenaltySeconds);
         
         // 中断されるまで繰り返す
-        while (currentTimeLimitSeconds > 0) {
+        while (session.CurrentTimeLimitSeconds > 0) {
 
             // カードを配る
-            Hand = Deal();
+            session.Deal();
 
             // 手札をクリア
             handController.ClearCards();
 
             // カード表示
-            handController.PlaceCards(Hand.Cards);
+            handController.PlaceCards(session.Hand.Cards);
 
             // 選択肢を用意する
-            var choices = GenerateChoices(Hand.HCP);
+            var choices = session.GenerateChoices();
             // 選択肢表示
             answerButtonController.SetChoices(choices);
 
@@ -99,14 +90,13 @@ public class GameController : MonoBehaviour
                 // 中断時はdo-whileを抜ける
                 if (!playerHasAnswered) break;
 
-                answerIsCorrect = Hand.HCP == answer;
-
-                if (answerIsCorrect)
+                if (session.AnswerIsCorrect)
                 {
                     playerHasAnswered = true;
 
-                    GainScore(Hand.HighCardCount);
+                    session.GainScore();
 
+                    // この辺HUD関連オブジェクトにまとめる
                     // 〇を表示
                     correctOverlay.GetComponent<Image>().SetOpacity(1);
                     // 正解の〇をフェードアウトさせる
@@ -117,11 +107,9 @@ public class GameController : MonoBehaviour
                 else
                 {
                     // 失敗時ペナルティ
-                    currentTimeLimitSeconds -= wrongAnswerPenaltySeconds;
                     audioSource.PlayOneShot(WrongAnswerAudioClip);
-                    timeLimitAreaController.DisplayPenalty(wrongAnswerPenaltySeconds);
                 }
-            } while (ShouldProceedToNextTurn());
+            } while (session.ShouldProceedToNextTurn());
         }
 
         // 時間切れになったらゲームオーバー
@@ -131,7 +119,7 @@ public class GameController : MonoBehaviour
 
         var modalWindow = GameObject.Find("ResultModal").GetComponent<ModalWindowManager>();
         modalWindow.titleText = "Result";
-        modalWindow.descriptionText = $"Score: {score}\nMiss: 0";
+        modalWindow.descriptionText = $"Score: {session.Score}\nMiss: 0";
         modalWindow.UpdateUI();
         modalWindow.OpenWindow();
 
@@ -141,134 +129,28 @@ public class GameController : MonoBehaviour
         audioSource.Play();
     }
 
-    private bool ShouldProceedToNextTurn()
-    {
-        return !answerIsCorrect;
-    }
-
-    private void GainScore(int gain)
-    {
-        score += gain;
-        scoreAreaController.UpdateScore(score);
-        scoreAreaController.DisplayGain(gain);
-    }
-
     private void Update()
     {
         // カウントダウン＆残り時間更新
-        currentTimeLimitSeconds -= Time.deltaTime;
-        timeLimitAreaController.UpdateTimeLimit(currentTimeLimitSeconds);
+        session.Update();
+        timeLimitAreaController.UpdateTimeLimit(session.CurrentTimeLimitSeconds);
     }
+    #endregion
 
+
+    #region Methods
     private IEnumerator WaitPlayerToAnswer()
     {
-        while (!playerHasAnswered && currentTimeLimitSeconds > 0)
+        while (!playerHasAnswered && session.CurrentTimeLimitSeconds > 0)
         {
             yield return 0;
         }
         Debug.Log("プレイヤーが回答しました");
     }
 
-    #endregion
-
-    
-    #region Methods
     public void LoadTitle()
     {
         SceneManager.LoadScene("Title");
-    }
-
-
-    /// <summary>
-    /// カードを配る
-    /// </summary>
-    /// <returns>手札</returns>
-    Hand Deal()
-    {
-        var deck = new Deck();
-
-        // 既に配ったカード
-        var pickedCardsId = new HashSet<int>();
-
-        var deal = new List<Card>();
-
-        foreach (int _ in Enumerable.Range(0, dealCount)) 
-        {
-            var card = PickCard(pickedCardsId, deck);
-            deal.Add(card);
-            pickedCardsId.Add(card.Id);
-        }
-
-        deal.Sort(Card.CompareBySuitAndNumber);
-        deal.Reverse();
-
-        return new Hand(deal);
-    }
-
-    /// <summary>
-    /// デッキからランダムにカードを引く
-    /// </summary>
-    /// <param name="pickedCardsId">すでに引いたカード</param>
-    /// <param name="deck">デッキ</param>
-    /// <returns>まだ引いてないカード</returns>
-    Card PickCard(HashSet<int> pickedCardsId, Deck deck)
-    {
-        seed++;
-        UnityEngine.Random.InitState(seed);
-        var id = UnityEngine.Random.Range(1, deck.Cards.Count);
-        var card = deck.Cards.Where(c => c.Id == id).Single();
-
-        if (!pickedCardsId.Contains(card.Id))
-        {
-            return card;
-        }
-        return PickCard(pickedCardsId, deck);
-    }
-
-    /// <summary>
-    /// 答えの候補を4種類出す
-    /// 正の数であること
-    /// </summary>
-    /// <param name="answer"></param>
-    /// <returns></returns>
-    List<int> GenerateChoices(int answer)
-    {
-        if (!answer.IsValidHCP())
-        {
-            throw new ArgumentException("answerの値が負か37より大きいです");
-        }
-
-        var choices = new List<int>();
-        choices.Add(answer);
-        while (choices.Count < 4)
-        {
-            choices.Add(GenerateChoice(answer, choices));
-        }
-        choices.Sort();
-        return choices;
-    }
-
-    /// <summary>
-    /// ユーザが選択する答えの候補を返す。
-    /// 負の数や37以上や他の選択肢と重複していた場合は、有効な数が出るまで引き直す
-    /// </summary>
-    /// <param name="answer">正しい答え</param>
-    /// <param name="choices">既に挙がっている答えの候補</param>
-    /// <returns></returns>
-    int GenerateChoice(int answer, List<int> choices)
-    {
-        seed++;
-        UnityEngine.Random.InitState(seed);
-        var choice = UnityEngine.Random.Range(0, 37);
-        if (!choice.IsValidHCP()
-            || choices.Contains(choice))
-        {
-            return GenerateChoice(answer, choices);
-        }
-        else
-        {
-            return choice;
-        }
     }
 
     /// <summary>
@@ -277,7 +159,7 @@ public class GameController : MonoBehaviour
     /// <param name="answer"></param>
     public void OnAnswer(int answer)
     {
-        this.answer = answer;
+        session.Answer(answer);
         playerHasAnswered = true;
     }
 
---
+++ Assets/HCPDrill/Models/Session.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+/// <summary>
+/// ゲームスタートからゲーム終了の間までの状態を管理する。
+/// ゲームセッションのルールを表現する。
+/// </summary>
+public class Session
+{
+    internal Hand Hand { get; set; }
+    internal int Score { get; set; }
+
+    internal float CurrentTimeLimitSeconds {
+        get {
+            return currentTimeLimitSeconds;
+        }
+    }
+
+    // 手札枚数
+    private readonly int dealCount = 13;
+
+    private float currentTimeLimitSeconds;
+    private readonly float wrongAnswerPenaltySeconds;
+
+    private readonly ScoreAreaController scoreAreaController;
+    private readonly TimeLimitAreaController timeLimitAreaController;
+
+    private int answer;
+    // 乱数シード
+    int seed;
+
+    public Session(ScoreAreaController scoreAreaController,
+        TimeLimitAreaController timeLimitAreaController,
+        int initialTimeLimitSeconds,
+        float wrongAnswerPenaltySeconds) 
+    {
+        this.scoreAreaController = scoreAreaController;
+        this.timeLimitAreaController = timeLimitAreaController;
+        currentTimeLimitSeconds = initialTimeLimitSeconds;
+        this.wrongAnswerPenaltySeconds = wrongAnswerPenaltySeconds;
+
+        Score = 0;
+
+        seed = DateTime.Now.Millisecond;
+
+    }
+
+    internal void Update()
+    {
+        currentTimeLimitSeconds -= Time.deltaTime;
+    }
+
+    /// <summary>
+    /// カードを配る
+    /// </summary>
+    /// <returns>手札</returns>
+    internal void Deal()
+    {
+        var deck = new Deck();
+
+        // 既に配ったカード
+        var pickedCardsId = new HashSet<int>();
+
+        var deal = new List<Card>();
+
+        foreach (int _ in Enumerable.Range(0, dealCount))
+        {
+            var card = PickCard(pickedCardsId, deck);
+            deal.Add(card);
+            pickedCardsId.Add(card.Id);
+        }
+
+        deal.Sort(Card.CompareBySuitAndNumber);
+        deal.Reverse();
+
+        this.Hand = new Hand(deal);
+    }
+
+
+    /// <summary>
+    /// デッキからランダムにカードを引く
+    /// </summary>
+    /// <param name="pickedCardsId">すでに引いたカード</param>
+    /// <param name="deck">デッキ</param>
+    /// <returns>まだ引いてないカード</returns>
+    Card PickCard(HashSet<int> pickedCardsId, Deck deck)
+    {
+        seed++;
+        UnityEngine.Random.InitState(seed);
+        var id = UnityEngine.Random.Range(1, deck.Cards.Count);
+        var card = deck.Cards.Where(c => c.Id == id).Single();
+
+        if (!pickedCardsId.Contains(card.Id))
+        {
+            return card;
+        }
+        return PickCard(pickedCardsId, deck);
+    }
+
+    /// <summary>
+    /// 答えの候補を4種類出す
+    /// 正の数であること
+    /// </summary>
+    /// <param name="answer"></param>
+    /// <returns></returns>
+    internal List<int> GenerateChoices()
+    {
+        var correctAnswer = Hand.HCP;
+        var choices = new List<int>();
+        choices.Add(correctAnswer);
+        while (choices.Count < 4)
+        {
+            choices.Add(GenerateChoice(choices));
+        }
+        choices.Sort();
+        return choices;
+    }
+
+    /// <summary>
+    /// ユーザが選択する答えの候補を返す。
+    /// 負の数や37以上や他の選択肢と重複していた場合は、有効な数が出るまで引き直す
+    /// </summary>
+    /// <param name="answer">正しい答え</param>
+    /// <param name="choices">既に挙がっている答えの候補</param>
+    /// <returns></returns>
+    int GenerateChoice(List<int> choices)
+    {
+        seed++;
+        UnityEngine.Random.InitState(seed);
+        var choice = UnityEngine.Random.Range(0, 37);
+        if (!choice.IsValidHCP()
+            || choices.Contains(choice))
+        {
+            return GenerateChoice(choices);
+        }
+        else
+        {
+            return choice;
+        }
+    }
+
+
+    internal void OnWrongAnswer()
+    {
+        currentTimeLimitSeconds -= wrongAnswerPenaltySeconds;
+        timeLimitAreaController.DisplayPenalty(wrongAnswerPenaltySeconds);
+    }
+
+    internal void Answer(int answer) {
+        this.answer = answer;
+    }
+
+    internal bool AnswerIsCorrect {
+        get {
+            return Hand.HCP == answer;
+        } 
+    }
+
+    internal bool ShouldProceedToNextTurn()
+    {
+        return !AnswerIsCorrect;
+    }
+
+    internal void GainScore()
+    {
+        var gain = Hand.HighCardCount;
+        Score += gain;
+        scoreAreaController.UpdateScore(Score);
+        scoreAreaController.DisplayGain(gain);
+    }
+
+
+}
